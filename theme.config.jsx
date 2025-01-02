ecc28701d4a07f04c4b5aa82bb239f98e5f4082a
SolistaHu <solistahu@gmail.com>
2025-01-02 14:04:19 +0800

    init project

---
+++ theme.config.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default {
+  footer: <p>MIT 2023 © Nextra.</p>,
+  head: ({ title, meta }) => (
+    <>
+      {meta.description && (
+        <meta name="description" content={meta.description} />
+      )}
+      {meta.tag && <meta name="keywords" content={meta.tag} />}
+      {meta.author && <meta name="author" content={meta.author} />}
+    </>
+  ),
+  readMore: 'Read More →',
+  postFooter: null,
+  darkMode: false,
+  navs: [
+    {
+      url: 'https://github.com/shuding/nextra',
+      name: 'Nextra'
+    }
+  ]
+}
